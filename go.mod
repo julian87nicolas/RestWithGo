f58c7bbd53879a2d05f2e2abea32923b9361e0a7
Julian Camargo <julicmrgo@gmail.com>
2025-12-03 17:28:54 -0300

    initial commit, models, modules and main

---
+++ go.mod
@@ -0,0 +1,13 @@
+module platzi.com/go/rest-ws
+
+go 1.25.4
+
+require (
+	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
+	github.com/gorilla/mux v1.8.1 // indirect
+	github.com/gorilla/websocket v1.5.3 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/lib/pq v1.10.9 // indirect
+	github.com/segmentio/ksuid v1.0.4 // indirect
+	golang.org/x/crypto v0.45.0 // indirect
+)
---
+++ server/server.go
@@ -0,0 +1,66 @@
+package server
+
+import (
+	"context"
+	"errors"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	database "platzi.com/go/rest-ws/database"
+	repository "platzi.com/go/rest-ws/repository"
+)
+
+type Config struct {
+	Port        string
+	JWTSecret   string
+	DatabaseUrl string
+}
+
+type Server interface {
+	Config() *Config
+}
+
+type Broker struct {
+	config *Config
+	router *mux.Router
+}
+
+func (b *Broker) Config() *Config {
+	return b.config
+}
+
+func NewServer(ctx context.Context, config *Config) (*Broker, error) {
+	if config.Port == "" {
+		return nil, errors.New("port is required")
+	}
+
+	if config.JWTSecret == "" {
+		return nil, errors.New("a secret is required")
+	}
+
+	if config.DatabaseUrl == "" {
+		return nil, errors.New("database is required")
+	}
+
+	broker := &Broker{
+		config: config,
+		router: mux.NewRouter(),
+	}
+
+	return broker, nil
+}
+
+func (b *Broker) Start(binder func(s Server, r *mux.Router)) {
+	b.router = mux.NewRouter()
+	binder(b, b.router)
+	repo, err := database.NewPostgresRepository(b.config.DatabaseUrl)
+	if err != nil {
+		log.Fatal(err)
+	}
+	repository.SetRepository(repo)
+	log.Println("Starting server on port", b.Config().Port)
+	if err := http.ListenAndServe(b.config.Port, b.router); err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
