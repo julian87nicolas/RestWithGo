7e825c590261351f5e815c0a074c760e9082a2ce
Julian Camargo <julicmrgo@gmail.com>
2025-12-03 17:54:21 -0300

    feat: added middleware

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"github.com/gorilla/mux"
 	"github.com/joho/godotenv"
 	"platzi.com/go/rest-ws/handlers"
+	"platzi.com/go/rest-ws/middleware"
 	"platzi.com/go/rest-ws/server"
 )
 
@@ -37,7 +38,10 @@ func main() {
 }
 
 func BindRoutes(s server.Server, r *mux.Router) {
+	r.Use(middleware.CheckAuthMiddleware())
+
 	r.HandleFunc("/", handlers.HomeHandler(s)).Methods(http.MethodGet)
 	r.HandleFunc("/signup", handlers.SignUpHandler(s)).Methods(http.MethodPost)
 	r.HandleFunc("/login", handlers.LoginHandler(s)).Methods(http.MethodPost)
+	r.HandleFunc("/me", handlers.MeHandler(s)).Methods(http.MethodGet)
 }
