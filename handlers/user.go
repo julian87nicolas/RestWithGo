1ca4387ff2e64ebf7f90940bd208d6e0bcf4aa9c
Julian Camargo <julicmrgo@gmail.com>
2025-12-03 17:29:40 -0300

    feat: database, handlers and repositories

---
+++ handlers/user.go
@@ -0,0 +1,114 @@
+package handlers
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/golang-jwt/jwt"
+	"github.com/segmentio/ksuid"
+	"golang.org/x/crypto/bcrypt"
+	"platzi.com/go/rest-ws/models"
+	"platzi.com/go/rest-ws/repository"
+	"platzi.com/go/rest-ws/server"
+)
+
+type SignUpLoginRequest struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+const (
+	HASH_COST = 8
+)
+
+type SignUpResponse struct {
+	Email string `json:"email"`
+	Id    string `json:"id"`
+}
+
+type LoginResponse struct {
+	Token string `json:"token"`
+}
+
+func SignUpHandler(s server.Server) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var request = SignUpLoginRequest{}
+		err := json.NewDecoder(r.Body).Decode(&request)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		hashedPassword, err := bcrypt.GenerateFromPassword([]byte(request.Password), HASH_COST)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		id, err := ksuid.NewRandom()
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		var user = models.User{
+			Email:    request.Email,
+			Password: string(hashedPassword),
+			Id:       id.String(),
+		}
+
+		err = repository.InsertUser(r.Context(), &user)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		w.Header().Set("content-type", "application/json")
+		json.NewEncoder(w).Encode(SignUpResponse{
+			Email: user.Email,
+			Id:    user.Id,
+		})
+	}
+}
+
+func LoginHandler(s server.Server) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var request = SignUpLoginRequest{}
+		err := json.NewDecoder(r.Body).Decode(&request)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		user, err := repository.GetUserByEmail(r.Context(), request.Email)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		if user == nil {
+			if err != nil {
+				http.Error(w, "invalid credentials", http.StatusUnauthorized)
+				return
+			}
+		}
+
+		if err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(request.Password)); err != nil {
+			log.Println(err)
+			http.Error(w, "invalid credentials", http.StatusUnauthorized)
+			return
+		}
+		claims := models.AppClaims{
+			UserId: user.Id,
+			StandardClaims: jwt.StandardClaims{
+				ExpiresAt: time.Now().Add(2 * time.Hour * 24).Unix(),
+			},
+		}
+		token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+		tokenString, err := token.SignedString([]byte(s.Config().JWTSecret))
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(LoginResponse{
+			Token: tokenString,
+		})
+	}
+}
---
+++ repository/user.go
@@ -0,0 +1,37 @@
+package repository
+
+import (
+	"context"
+
+	"platzi.com/go/rest-ws/models"
+)
+
+type UserRepository interface {
+	InsertUser(ctx context.Context, user *models.User) error
+	GetUserById(ctx context.Context, id string) (*models.User, error)
+	GetUserByEmail(ctx context.Context, email string) (*models.User, error)
+	Close() error
+}
+
+var implementation UserRepository
+
+func SetRepository(repository UserRepository) {
+	implementation = repository
+}
+
+func InsertUser(ctx context.Context, user *models.User) error {
+	return implementation.InsertUser(ctx, user)
+}
+
+func GetUserById(ctx context.Context, id string) (*models.User, error) {
+	return implementation.GetUserById(ctx, id)
+}
+
+func GetUserByEmail(ctx context.Context, email string) (*models.User, error) {
+	return implementation.GetUserByEmail(ctx, email)
+}
+
+func Close() error {
+	err := implementation.Close()
+	return err
+}
